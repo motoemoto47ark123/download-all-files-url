f9dbec1e8e15c8e20d8fd65760cfe91e062fde53
motoemoto47ark123 <121888254+motoemoto47ark123@users.noreply.github.com>
2023-05-06 21:58:35 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Ask the user for the URL
+read -p "Enter the URL: " url
+
 # Use wget to download all files on the site
-wget -r -np -nH --cut-dirs=1 -P photos url go here
+wget -r -np -nH --cut-dirs=1 -P site-files "$url"
 
 # Print a success message
 echo "Download complete."
